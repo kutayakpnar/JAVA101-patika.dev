f3f3c22aff783e8cb97c1e88b0a9b9aee9165841
Kutay Akpinar <kutayakpnar@gmail.com>
2023-02-12 14:56:30 +0300

    Calculating Area

---
+++ untitled/src/CalculatingGrades.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class CalculatingGrades {
+    public static void main(String[] args) {
+
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter math grades:");
+        int mat= scan.nextInt();
+        scan.nextLine();
+        System.out.println("Enter physic grades:");
+        int phy= scan.nextInt();
+        scan.nextLine();
+        System.out.println("Enter chem grades:");
+        int chem= scan.nextInt();
+        scan.nextLine();
+        System.out.println("Enter hist grades:");
+        int hist= scan.nextInt();
+        scan.nextLine();
+        System.out.println("Enter music grades:");
+        int mus= scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Please enter the number of lesson");
+        int lesson= scan.nextInt();
+
+        double average=mat+phy+chem+hist+mus;
+        double result=average/lesson;
+
+        System.out.println("Your gpa is: "+result);
+    }
+}
