85efa8439b964fe1b4f6f28eb5e1d926ca029eb4
Kutay Akpinar <kutayakpnar@gmail.com>
2023-02-16 02:13:30 +0300

    Employee salary calculator basic

--- SalaryCalculation/src/Employee.java
+++ SalaryCalculation/src/Employee.java
@@ -3,55 +3,61 @@ public class Employee {
     double salary;
     int workHours;
     double hireYear;
-    double salaryW;
+
 
     Employee(String name,double salary,int workHours,double hireYear){
         this.name=name;
         this.salary=salary;
         this.workHours=workHours;
         this.hireYear=hireYear;
-        this.salaryW=salary;
+
     }
-    void tax(){
-        double tax;
+    double tax(){
+        double tax=0;
         if(this.salary<=1000){
             System.out.println("No tax.");
-            tax=0;
+            return 0;
 
         }
         else{
             tax=this.salary*0.03;
-            System.out.println("TOTAL TAX: "+tax);
-            this.salaryW=this.salaryW-tax;
+            //System.out.println("TOTAL TAX: "+tax);
+
         }
+        return tax;
     }
-    void bonus(){
+    double bonus(){
         double bonus = 0;
         if(this.workHours>40){
             bonus += (workHours-40)*30;
-            this.salaryW+=bonus;
+
         }
         System.out.println("Bonus:"+bonus);
+        return bonus;
+
     }
-    void raiseSalary(){
+    double raiseSalary(){
         double totalYear=2021-this.hireYear;
+        double raise=0;
         if (totalYear<=9){
-            this.salary+=this.salary*0.05;
-            System.out.println("Raise salary:"+ this.salary*0.05);
+            raise=this.salary*0.05;
+
         }
         else if (totalYear>9 && totalYear<=19){
-            this.salary+=this.salary*0.10;
-            System.out.println("Raise salary:"+ this.salary*0.1);
+            raise=this.salary*0.10;
+
         }
         else if (totalYear>19){
-            this.salary+=this.salary*0.15;
-            System.out.println("Raise salary:"+ this.salary*0.15);
+            raise=this.salary*0.15;
+
         }
+        return raise;
     }
     public String toString(){
         return "Name: "+this.name+"\n"+
-        "Total Salary without Tax: "+this.salary+"\n"+
-        "Salary with Tax and Bonus:"+this.salaryW+"\n"+
+        "Total Salary without Tax: "+(this.salary-this.tax()+this.bonus())+"\n"+
+        "Maaş artışı:"+this.raiseSalary()+"\n"+
+        "Salary with Tax and Bonus:"+(this.salary+this.raiseSalary())+"\n"+
         "Work Hours: "+this.workHours+"\n"+
         "Hire year: "+this.hireYear+"\n";
 
